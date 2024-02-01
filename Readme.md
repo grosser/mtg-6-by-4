4e7c1ec84f196a8b575f33e778ed06a163168b7a
Michael Grosser <michael@grosser.it>
2024-01-31 21:26:49 -0800

    adjust ppi so printing is exactly right size

--- Readme.md
+++ Readme.md
@@ -3,4 +3,4 @@ Convert mtgprint.net output to 6x4 to send to photo print
 - download your deck as deck.pdf
 - drop into this folder
 - run `ruby split.rb`
-- print `combined-*.png` at your local drug store
+- print `print/*.png` at your local drug store
--- split.rb
+++ split.rb
@@ -6,25 +6,27 @@ def sh(command)
 end
 
 ppi = 300
-width = (2.48 * ppi).to_i
-height = (3.48 * ppi).to_i
+width = (2.48 * ppi).to_i # should be 2.5 but images are too small
+height = (3.48 * ppi).to_i # should be 3.5 but images are too small
 xoffset = 125
 yoffset = 290
+out_ppi = ppi * 1.03 # when printed cards are slightly too big even though our math looks good
 
-sh "rm -rf *.png"
-sh "convert -density #{ppi} deck.pdf page-%d.png"
+# sh "rm -rf **/*.png"
+sh "mkdir -p pages cards print"
+sh "convert -density #{ppi} deck.pdf pages/%d.png"
 
-Dir["page-*.png"].each_with_index do |png, pi|
+Dir["pages/*.png"].each_with_index do |png, pi|
   3.times do |ri|
     3.times do |ci|
       ii = (pi * 9) + (ri * 3) + ci
-      sh "convert #{png} -crop #{width}x#{height}+#{xoffset + ci * width}+#{yoffset + ri * height} image-#{ii}.png"
+      sh "convert #{png} -crop #{width}x#{height}+#{xoffset + ci * width}+#{yoffset + ri * height} cards/#{ii}.png"
     end
   end
 end
 
-Dir["image-*.png"].sort_by { |n| Integer(n[/\d+/]) }.each_slice(2).each_with_index.each do |(a, b), i|
-  out = "combined-#{i}.png"
-  sh "convert #{a} #{b} +append -units PixelsPerInch -density 300 #{out}"
-  sh "convert #{out} -gravity center -background white -extent #{6*ppi}x#{4*ppi} #{out}"
+Dir["cards/*.png"].sort_by { |n| Integer(n[/\d+/]) }.each_slice(2).each_with_index.each do |(a, b), i|
+  out = "print/#{i}.png"
+  sh "convert #{a} #{b} +append -units PixelsPerInch -density #{out_ppi} #{out}"
+  sh "convert #{out} -gravity center -background white -extent #{6*out_ppi}x#{4*out_ppi} #{out}"
 end
