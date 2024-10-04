c8f8cd9b9778482bbfb02b24b0263cc48cdc688f
Michael Grosser <michael@grosser.it>
2024-10-03 20:04:30 -0700

    deprecations

--- split.rb
+++ split.rb
@@ -14,19 +14,19 @@ out_ppi = ppi * 1.03 # when printed cards are slightly too big even though our m
 
 sh "rm -rf {pages,cards,print}/*.png"
 sh "mkdir -p pages cards print"
-sh "magick convert -density #{ppi} deck.pdf pages/%d.png"
+sh "magick -density #{ppi} deck.pdf pages/%d.png"
 
 Dir["pages/*.png"].each_with_index do |png, pi|
   3.times do |ri|
     3.times do |ci|
       ii = (pi * 9) + (ri * 3) + ci
-      sh "convert #{png} -crop #{width}x#{height}+#{xoffset + ci * width}+#{yoffset + ri * height} cards/#{ii}.png"
+      sh "magick #{png} -crop #{width}x#{height}+#{xoffset + ci * width}+#{yoffset + ri * height} cards/#{ii}.png"
     end
   end
 end
 
 Dir["cards/*.png"].sort_by { |n| Integer(n[/\d+/]) }.each_slice(2).each_with_index.each do |(a, b), i|
   out = "print/#{i}.png"
-  sh "convert #{a} #{b} +append -units PixelsPerInch -density #{out_ppi} #{out}"
-  sh "convert #{out} -gravity center -background white -extent #{6*out_ppi}x#{4*out_ppi} #{out}"
+  sh "magick #{a} #{b} +append -units PixelsPerInch -density #{out_ppi} #{out}"
+  sh "magick #{out} -gravity center -background white -extent #{6*out_ppi}x#{4*out_ppi} #{out}"
 end
