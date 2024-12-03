ea09cf905d19a31fe5c6d78d700141d3484973e0
Michael Grosser <michael@grosser.it>
2024-12-03 13:32:01 -0800

    layout changed

--- split.rb
+++ split.rb
@@ -8,8 +8,8 @@ end
 ppi = 300
 width = (2.475 * ppi).round # should be 2.5 but images are too small
 height = (3.46 * ppi).round # should be 3.5 but images are too small
-xoffset = 125
-yoffset = 291
+xoffset = 160
+yoffset = 91
 out_ppi = ppi * 1.03 # when printed cards are slightly too big even though our math looks good
 
 sh "rm -rf {pages,cards,print}/*.png"
